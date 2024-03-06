c735f29db4cd0a64309b0e7eec3e05309f69faa6
Lolitron-0 <ovcharov_05@mail.ru>
2024-03-06 03:13:07 +0300

    23 current  2024-03-06 03:12:56  24.05.20240303.b8697e5  6.6.19                          *

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -17,6 +17,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
