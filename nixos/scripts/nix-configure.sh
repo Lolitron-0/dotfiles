9bb18487cb8cb36dacc9b0083c9ae2c8c7a62dad
Lolitron-0 <ovcharov_05@mail.ru>
2024-03-06 03:14:12 +0300

    23 current  2024-03-06 03:12:56  24.05.20240303.b8697e5  6.6.19                          *

--- nixos/scripts/nix-configure.sh
+++ nixos/scripts/nix-configure.sh
@@ -6,6 +6,15 @@ pushd ~/dotfiles
 
 $EDITOR
 
-nrb
+echo "NixOS rebuilding..."
+
+sudo nixos-rebuild switch --flake ~/dotfiles/nixos#default
+
+current=$(nixos-rebuild list-generations | grep current)
+
+git add .
+git commit -am "$current"
 
 popd
+
+notify-send -e "NixOS rebuilt OK!" -t 600 --icon=software-update-available
