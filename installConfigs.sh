a4b87313799d113a4d2af70104268b54b3a9bd3e
Lolitron-0 <ovcharov_05@mail.ru>
2024-08-22 12:28:25 +0300

    zsh

--- installConfigs.sh
+++ installConfigs.sh
@@ -15,6 +15,8 @@ rm -rf ~/.config/powerline-shell
 ln -s ~/dotfiles/nixos/modules/home-manager/features/nvim ~/.config
 ln -s ~/dotfiles/nixos/modules/home-manager/features/kitty ~/.config
 ln -s ~/dotfiles/nixos/modules/home-manager/features/powerline-shell ~/.config
+ln -s ~/dotfiles/nixos/modules/home-manager/features/zsh/.zshrc ~
+ln -s ~/dotfiles/nixos/modules/home-manager/features/zsh/.oh-my-zsh ~
 
 echo "Links created"
 
