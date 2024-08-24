38aab2396b126cf561063a14593bc5d8705fdf68
niten <vovcharov@helene.cluster.local>
2024-08-24 15:40:13 +0000

    minor

--- installConfigs.sh
+++ installConfigs.sh
@@ -7,6 +7,7 @@ echo "Don't forget to add powerline!"
 # if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
 #     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 # fi
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 rm -rf ~/.config/nvim
 rm -rf ~/.config/kitty
@@ -16,6 +17,7 @@ ln -s ~/dotfiles/nixos/modules/home-manager/features/nvim ~/.config
 ln -s ~/dotfiles/nixos/modules/home-manager/features/kitty ~/.config
 ln -s ~/dotfiles/nixos/modules/home-manager/features/powerline-shell ~/.config
 rm ~/.zshrc
+rm -r ~/.oh-my-zsh/custom
 ln -s ~/dotfiles/nixos/modules/home-manager/features/zsh/.zshrc ~
 ln -s ~/dotfiles/nixos/modules/home-manager/features/zsh/custom ~/.oh-my-zsh
 
