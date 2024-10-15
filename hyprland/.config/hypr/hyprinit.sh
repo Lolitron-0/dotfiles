b0c9a0594d71362772d94bde0d758d638cc94ac1
Lolitron-0 <ovcharov_05@mail.ru>
2024-10-15 14:05:28 +0300

    paths hotfix

--- hyprland/.config/hypr/hyprinit.sh
+++ hyprland/.config/hypr/hyprinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 swww-daemon --no-cache &
 
-cfg_dir=/home/niten/dotfiles/nixos/modules/home-manager/features/hyprland
+cfg_dir=$HOME/.config/hypr
 
 swww img ${cfg_dir}/Snow-valley.jpg
 sleep 3
