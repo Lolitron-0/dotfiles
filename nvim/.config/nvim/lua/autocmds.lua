e1c346eeb446a5a0924600fcf775f63ee3ad783a
Lolitron-0 <ovcharov_05@mail.ru>
2024-12-01 13:34:11 +0300

    hyprland conf update + autocmd

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -22,15 +22,15 @@ end
 vim.api.nvim_create_autocmd({ "BufEnter" }, { callback = open_nvim_tree })
 
 -- Save NvimTree in auto-session
--- vim.api.nvim_create_autocmd({ 'BufEnter' }, {
--- 	pattern = 'NvimTree*',
--- 	callback = function()
--- 		local view = require('nvim-tree.view')
--- 		local is_visible = view.is_visible()
---
--- 		local api = require('nvim-tree.api')
--- 		if not is_visible then
--- 			api.tree.open()
--- 		end
--- 	end,
--- })
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+	pattern = 'NvimTree*',
+	callback = function()
+		local view = require('nvim-tree.view')
+		local is_visible = view.is_visible()
+
+		local api = require('nvim-tree.api')
+		if not is_visible then
+			api.tree.open()
+		end
+	end,
+})
