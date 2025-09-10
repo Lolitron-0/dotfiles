d1d8c6fba6d845a6ac2ad6e8bd68f5ca860862a0
niten01 <ovcharov_05@mail.ru>
2025-09-10 11:45:17 +0300

    fix tmux unicode

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,6 @@
 alias ls=lsd
 alias ll="lsd -la"
 alias gt="xclip -sel c < ~/dev/git_token && wl-copy < ~/dev/git_token"
-alias tmux="tmux -2"
+alias tmux="tmux -2u"
 alias cloc-cpp="cloc --exclude-dir=build,thirdparty,submodules --include-lang=C++,C,'C/C++ Header,CMake' ."
 alias cppinit="~/.oh-my-zsh/custom/cppinit/cppinit.sh"
