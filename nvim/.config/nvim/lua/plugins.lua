10eb69946e4a7f56a4fc90c5a0dd2a1ad537fe3f
niten01 <ovcharov_05@mail.ru>
2025-09-16 11:41:01 +0300

    fall theme

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -142,7 +142,7 @@ return packer.startup(function(use)
     event = "VimEnter",
     config = function()
       require("drop").setup {
-        theme = "spring", -- "leaves", "snow", "stars", "xmas", "spring", "summer"
+        theme = "leaves", -- "leaves", "snow", "stars", "xmas", "spring", "summer"
         filetypes = { --[[ "dashboard", ]] "alpha", "starter" },
       }
     end,
